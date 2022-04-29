2a7d63258024bee78dc7fba8019b6a132b163249
pawelek2000 <65534106+pawelek2000@users.noreply.github.com>
2022-04-29 22:56:20 +0200

    Add files via upload

---
+++ Zadanie 1/zadanie1-wpf/MVVM/Model/Maszyna.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie1_wpf.MVVM.Model
+{
+    
+    public class Maszyna
+    {
+        private List<string> Tablica { get; set; }
+
+        public Maszyna()
+        {
+            Tablica = new List<string>();
+        }
+        public void Dodaj(string kupon) 
+        {
+            Tablica.Add(kupon);
+        }
+        public string Wyjmij() 
+        {
+            Random random = new Random();
+            var randomIndex = random.Next(0,Tablica.Count-1);
+            var tempVal = Tablica[randomIndex];
+            Tablica.RemoveAt(randomIndex);
+            return tempVal;
+        }
+
+        public bool Pusta() 
+        {
+            if (Tablica.Count == 0)
+                return true;
+            else
+                return false;
+        }
+
+        public string Zawartosc() 
+        {
+            string zawartosc = "";
+            foreach (string kupon in Tablica) 
+            {
+                zawartosc +=kupon + ", ";  
+            }
+            return zawartosc;
+        }
+    }
+}
---
+++ Zadanie 1/zadanie1-wpf/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using zadanie1_wpf.Core;
+using zadanie1_wpf.MVVM.Model;
+
+namespace zadanie1_wpf.MVVM.ViewModel
+{
+    public class MainViewModel : ObservableObject
+    {
+        private Maszyna MaszynaLosujaca { get; set; }
+        public RelayCommand DodajCommand { get; set; }
+        public RelayCommand WyjmijComand { get; set; }
+
+        public MainViewModel()
+        {
+            MaszynaLosujaca = new Maszyna();
+            Zawartosc = MaszynaLosujaca.Zawartosc();
+
+            DodajCommand = new RelayCommand(o =>
+            {
+                MaszynaLosujaca.Dodaj(TextBoxValue);
+                Zawartosc = "Dodano: " + TextBoxValue + "\n" + "Kupony w maszynie: " + MaszynaLosujaca.Zawartosc();
+            });
+            WyjmijComand = new RelayCommand(o => 
+            {
+                if (MaszynaLosujaca.Pusta())
+                {
+                    Zawartosc = "Maszyna Losująca jest pusta.";
+                }
+                else 
+                {
+                    Zawartosc = "Wylosowany kupon: " + MaszynaLosujaca.Wyjmij();
+                }
+            });
+
+        }
+        public string Zawartosc
+        {
+            get { return _zawartosc; }
+            set
+            {
+                _zawartosc = value;
+                OnPropertyChanged();
+            }
+        }
+        private string _zawartosc;
+
+        public string TextBoxValue
+        {
+            get { return _textBoxValue; }
+            set
+            {
+                _textBoxValue = value;
+                OnPropertyChanged();
+            }
+        }
+        private string _textBoxValue;
+    }
+}
