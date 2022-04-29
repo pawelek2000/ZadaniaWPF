854e5010335f14af310cdb1b62a27ec9cbf08eb1
pawelek2000 <65534106+pawelek2000@users.noreply.github.com>
2022-04-29 22:57:11 +0200

    Add files via upload

---
+++ Zadanie 3/WpfMVVM/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfMVVM.Core;
+using WpfMVVM.MVVM.Model;
+
+namespace WpfMVVM.MVVM.ViewModel
+{
+    internal class MainViewModel :ObservableObject 
+    {
+        public RelayCommand odswiezCommand { get; set; }
+        public RelayCommand zamknijCommand { get; set; }
+        public RelayCommand zatwierdzCommand { get; set; }
+        public MainViewModel()
+        {
+            zamowienie = new Zamowienie();
+            odswiezCommand = new RelayCommand(o=> 
+            {
+                zamowienie.PodsumowanieZamowienia();
+            });
+
+            zamknijCommand = new RelayCommand(o => 
+            {
+                foreach (Window item in Application.Current.Windows)
+                {
+                    if (item.DataContext == this) item.Close();
+                }
+            });
+            zatwierdzCommand = new RelayCommand(o => 
+            {
+                MessageBox.Show("Zamównie przyjete");
+                zamowienie = new Zamowienie();
+            });
+        }
+
+        public Zamowienie zamowienie
+        {
+            get { return _zamowienie; }
+            set
+            {
+                _zamowienie = value;
+                OnPropertyChanged();
+            }
+        }
+        private Zamowienie _zamowienie;
+        
+       
+
+    }
+}
