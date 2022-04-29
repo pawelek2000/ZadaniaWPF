a0037886eb0250b12903b0a42cd8f4a1828f72da
pawelek2000 <65534106+pawelek2000@users.noreply.github.com>
2022-04-29 23:06:48 +0200

    Add files via upload

---
+++ Zadanie 6/WpfMVVM/MVVM/Model/Osoba.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfMVVM.Core;
+
+namespace Zadanie6.MVVM.Model
+{
+    internal class Osoba : ObservableObject
+    {
+        public int Id { get; set; }
+        public string Imie
+        {
+            get { return _Imie; }
+            set
+            {
+                
+                _Imie = value;
+                OnPropertyChanged();
+                AktualazacjaHeder();
+            }
+        }
+        private string _Imie;
+
+        public string Nazwisko
+        {
+            get { return _Nazwisko; }
+            set
+            {
+                _Nazwisko = value;
+                OnPropertyChanged();
+                AktualazacjaHeder();
+            }
+        }
+        private string _Nazwisko;
+
+        public string Email
+        {
+            get { return _Email; }
+            set
+            {
+                _Email = value;
+                OnPropertyChanged();
+                AktualazacjaHeder();
+            }
+        }
+        private string _Email;
+
+        public double Wyplata
+        {
+            get { return _Wyplata; }
+            set
+            {
+                _Wyplata = value;
+                OnPropertyChanged();
+            }
+        }
+        private double _Wyplata;
+        public Regiony IdRegionu
+        {
+            get { return _IdRegionu; }
+            set
+            {
+                _IdRegionu = value;
+                OnPropertyChanged();
+            }
+        }
+        private Regiony _IdRegionu;
+
+        public int PoziomDostepu
+        {
+            get { return _PoziomDostepu; }
+            set
+            {
+                _PoziomDostepu = value;
+                OnPropertyChanged();
+            }
+        }
+        private int _PoziomDostepu;
+
+        public bool DaneOsobowe
+        {
+            get { return _DaneOsobowe; }
+            set
+            {
+                _DaneOsobowe = value;
+                OnPropertyChanged();
+              
+            }
+        }
+        private bool _DaneOsobowe;
+        public string Header 
+        {
+            get { return _Header; }
+            set
+            {
+                _Header = value;
+                OnPropertyChanged();
+            }
+        }
+        private string _Header;
+
+        public bool IsAButton
+        {
+            get { return _IsAButton; }
+            set
+            {
+                _IsAButton = value;
+                OnPropertyChanged();
+                AktualazacjaHeder();
+            }
+        }
+        private bool _IsAButton;
+        private void AktualazacjaHeder() 
+        {
+            if (IsAButton)
+                Header = "dodaj nową osobę...";
+            else
+                Header = string.Format("{0} {1} ({2})", Imie, Nazwisko, Email);
+        }
+    }
+    
+}
---
+++ Zadanie 6/WpfMVVM/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfMVVM.Core;
+using Zadanie6.MVVM.Model;
+
+namespace WpfMVVM.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+        public RelayCommand DodajOsobaCommand { get; set; }
+        public RelayCommand UsunCommand { get; set; }
+        public ObservableCollection<Regiony> ListaRegionow { get; set; }
+        public MainViewModel()
+        {
+            ListaOsob = new ObservableCollection<Osoba>();
+            ListaRegionow = new ObservableCollection<Regiony> {new Regiony { Id = 0, Nazwa = "Białystok"}, new Regiony { Id = 1, Nazwa = "Warszawa" },
+                                                               new Regiony { Id = 0, Nazwa = "Gdańsk"}, new Regiony { Id = 0, Nazwa = "Wadowice"}};
+            
+            ListaOsob.Add(new Osoba
+            {
+                IsAButton = true,
+            });
+
+            DodajOsobaCommand = new RelayCommand(o =>
+            {
+                SelectedOsoba = (o as Osoba);
+
+                
+                if (SelectedOsoba.IsAButton) 
+                {
+                    SelectedOsoba.IsAButton = false;
+                    var index = (ListaOsob.Max(f => f.Id)) + 1;
+                    SelectedOsoba.Id = index;
+                    ListaOsob.Add(new Osoba
+                    {
+                        IsAButton = true,
+                    });
+
+                }
+
+                AktualizacajWidocznisci();
+            });
+            UsunCommand = new RelayCommand(o =>
+            {
+                ListaOsob.Remove(SelectedOsoba);
+                AktualizacajWidocznisci();
+            });
+
+
+        }
+        public ObservableCollection<Osoba> ListaOsob
+        {
+            get { return _ListaOsob; }
+            set
+            {   
+                _ListaOsob = value;
+                OnPropertyChanged();
+                
+            }
+        }
+        private ObservableCollection<Osoba> _ListaOsob;
+
+        public Osoba SelectedOsoba
+        {
+            get 
+            {   
+                return _SelectedOsoba; 
+            }
+            set
+            {
+                _SelectedOsoba = value;
+                OnPropertyChanged();
+            }
+        }
+        private Osoba _SelectedOsoba;
+
+        public bool CzyJestJakasOsoba
+        {
+            get { return _CzyJestJakasOsoba; }
+            set
+            {
+                _CzyJestJakasOsoba = value;
+                OnPropertyChanged();
+
+            }
+        }
+        private bool _CzyJestJakasOsoba;
+
+        private void AktualizacajWidocznisci() 
+        {
+            if (ListaOsob == null || ListaOsob.Count == 1)
+                CzyJestJakasOsoba = false;
+            else
+                CzyJestJakasOsoba = true;
+        }
+    }
+}
